ed7ef8a0b4070066ac38db2aa8e1cd3866eabf78
Brian Lee <brianwlee02@gmail.com>
2023-11-04 17:36:27 -0700

    Added RecipeSimple display class. Tested with sample recipes.

--- src/FrontEnd/RecipeList.java
+++ src/FrontEnd/RecipeList.java
@@ -1,15 +1,79 @@
 package FrontEnd;
 
 import javafx.scene.layout.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.image.*;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.*;
+import javafx.scene.control.Label;
 
+
+/**
+ * Simple recipe display for recipe list
+ */
+class RecipeSimple extends HBox{
+
+    private Recipe recipe;
+    private Label recipeName;
+    private Button viewButton;
+
+
+    RecipeSimple(Recipe r){
+        this.setPrefSize(Constants.WINDOW_WIDTH-100, 60);
+        this.setMinHeight(HBox.USE_PREF_SIZE);
+        this.setStyle(Constants.defaultRecipeColor);
+        this.setSpacing(10);
+        this.setPadding(new Insets(20));
+
+        recipe = r;
+
+        recipeName = new Label(r.getRecipeName());
+        recipeName.setStyle(Constants.defaultTextStyle);
+        recipeName.setAlignment(Pos.CENTER_LEFT);
+
+        Region growableRegion = new Region();
+        HBox.setHgrow(growableRegion, Priority.ALWAYS);
+
+        viewButton = new Button("View");
+        viewButton.setStyle(Constants.viewButtonStyle);
+        viewButton.setMinWidth(Button.USE_PREF_SIZE);
+        viewButton.setAlignment(Pos.CENTER_RIGHT);
+
+        this.getChildren().addAll(recipeName, growableRegion, viewButton);
+        this.setAlignment(Pos.CENTER);
+    }
+
+    public Button getViewButton(){
+        return viewButton;
+    }
+
+    public Recipe getRecipe(){
+        return recipe;
+    }
+}
 
 //TODO: Create RecipeList functionality
 public class RecipeList extends VBox{
     
     RecipeList() {
-        this.setSpacing(5); // sets spacing between recipes
+        this.setSpacing(4); // sets spacing between recipes
         this.setPrefSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
         this.setStyle(Constants.defaultBackgroundColor);
+
+        /**
+         * THIS IS JUST AN EXAMPLE
+         * TODO: Remove this and add actual functionality
+         */
+        for(int i = 0; i < 10; i++){
+            this.getChildren().add(new RecipeSimple(new Recipe("Chicken " + i, "3/4 chicken breast", "cook da chicken")));
+        }
+        
     }
 
 }
