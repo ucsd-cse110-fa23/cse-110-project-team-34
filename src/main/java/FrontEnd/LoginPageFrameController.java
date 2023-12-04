821dc81a1b41af563aea128e43af6ac3d3b9ca2e
Andy8131 <andy.liu0123456@gmail.com>
2023-12-04 15:40:02 -0800

    added empty action handlers

--- src/main/java/FrontEnd/LoginPageFrameController.java
+++ src/main/java/FrontEnd/LoginPageFrameController.java
@@ -1,5 +1,7 @@
 package FrontEnd;
 
+import javafx.event.ActionEvent;
+
 public class LoginPageFrameController {
     private LoginPageFrame view;
     private HTTPRequestModel model;
@@ -7,6 +9,17 @@ public class LoginPageFrameController {
     public LoginPageFrameController(LoginPageFrame view, HTTPRequestModel model) {
         this.view = view;
         this.model = model;
+
+        this.view.setAccountCreationButtonAction(this::handleAccountCreationButton);
+        this.view.setLoginButtonAction(this::handleLoginButton);
+
+    }
+
+    private void handleAccountCreationButton(ActionEvent event) {
+
+    }
+
+    private void handleLoginButton(ActionEvent event) {
 
     }
 }
