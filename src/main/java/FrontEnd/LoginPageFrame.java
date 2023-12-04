1db0a306c70c7a048d1a5e89c318d2665191b3c6
Brian Lee <brianwlee02@gmail.com>
2023-12-04 01:44:17 -0800

    Fixed Gradle Build Error Caused from database files moved to own subdirectory

--- src/main/java/FrontEnd/LoginPageFrame.java
+++ src/main/java/FrontEnd/LoginPageFrame.java
@@ -1,8 +1,5 @@
 package FrontEnd;
 
-import BackEnd.*; //This is only because we do not have a HTTP Server yet. Fix and remove asap
-import BackEnd.database.RecipeListDatabase;
-import BackEnd.database.UserDatabase;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
