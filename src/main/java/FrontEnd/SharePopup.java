226d5d438033deb06565dd6b16e26ff63876385c
Brian Lee <brianwlee02@gmail.com>
2023-12-04 19:20:23 -0800

    Added SharePopup and Share Button

---
+++ src/main/java/FrontEnd/SharePopup.java
@@ -0,0 +1,13 @@
+package FrontEnd;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+
+public class SharePopup extends BorderPane{
+    
+}
