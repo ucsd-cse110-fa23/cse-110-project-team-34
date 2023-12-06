cb8a5b419789ee764b78e422d1dac6488bd14f4d
Yuhao Zhang <yuhaozhang@Yuhaos-MacBook-Air.local>
2023-12-05 21:00:32 -0800

    added new tests

---
+++ src/test/java/MockChatGPT.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import BackEnd.api.ChatGPT;
+import FrontEnd.Recipe;
+
+public class MockChatGPT extends ChatGPT{
+    private Recipe mockedRecipe;
+
+    public void setMockedRecipe(Recipe mockedRecipe) {
+        this.mockedRecipe = mockedRecipe;
+    }
+
+    @Override
+    public String runChatGPT(String option) throws IOException, InterruptedException, URISyntaxException {
+        return "ChatGPT initiated";
+    }
+
+    @Override
+    public boolean verifyIngredients(String ingredients){
+        System.out.println("verifying ingredientd");
+        return true;
+    }
+
+    @Override
+    public String createRecipeAsJSONString(String ingredients, String mealType) throws IOException, InterruptedException, URISyntaxException{
+        return "Successfully created recipe as JSON string";
+    }
+
+    
+}
