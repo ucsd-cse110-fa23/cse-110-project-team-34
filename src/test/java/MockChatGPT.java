8d01fc4a7429bf188aed4f8c503d908bbf20a9bc
Yuz167 <yuhaozhang2002@163.com>
2023-12-05 23:12:40 -0800

    "added more ChatGPT tests"

--- src/test/java/MockChatGPT.java
+++ src/test/java/MockChatGPT.java
@@ -1,31 +1,42 @@
 import java.io.IOException;
 import java.net.URISyntaxException;
-
 import BackEnd.api.ChatGPT;
 import FrontEnd.Recipe;
 
+
+
 public class MockChatGPT extends ChatGPT{
-    private Recipe mockedRecipe;
+    /*private Recipe mockedRecipe;
 
     public void setMockedRecipe(Recipe mockedRecipe) {
         this.mockedRecipe = mockedRecipe;
-    }
+    }*/
 
     @Override
-    public String runChatGPT(String option) throws IOException, InterruptedException, URISyntaxException {
-        return "ChatGPT initiated";
+    public String runChatGPT(String option) throws IOException, InterruptedException, URISyntaxException{
+        return "ChatGPT initiated for prompt: " + option;
     }
 
     @Override
     public boolean verifyIngredients(String ingredients){
-        System.out.println("verifying ingredientd");
-        return false;
+        if(ingredients.equals("bad ingredients")){
+            return false;
+        }
+        return true;
     }
 
     @Override
-    public String createRecipeAsJSONString(String ingredients, String mealType) throws IOException, InterruptedException, URISyntaxException{
-        return "Successfully created recipe as JSON string";
+    public String createRecipeName(String ingredients, String mealType){
+        return "New recipe name for ingredients: "+ingredients+" and meal type: "+mealType;
     }
 
-    
+    @Override
+    public String createIngredientList(String ing, String recipeName){
+        return "Create new ingredient list for ingredients: "+ing+" with recipe name: "+recipeName;
+    }
+
+    @Override
+    public String createRecipeDirections(String recipeName, String ingredientList){
+        return "Create recipe directions for ingredients: "+ingredientList+" with recipe name: "+recipeName;
+    }
 }
